f057f87e12b835335d1db4168920560b6a0c3132
samwright000 <samualwright@outlook.com>
2021-11-01 12:54:14 +0000

    ox

--- newox.c
+++ newox.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdbool.h>
-#include<stdlib.h>
-
-
-
 void printBoard1D(char board[9]){
 
 	int i;
@@ -26,18 +22,16 @@ void printBoard1D(char board[9]){
 int makeMove(char board[9],int player, char player1Moves[9], char player2Moves[9]){
 
 	int location; 
-	char locationstring;
-	
+	char locationchar;
 	while (1)
 	{
 	
-		printf("Please Enter Location Player %d: \n",player);
-		scanf("%c",&locationstring);
-		scanf("%i",&location);	
+		printf("Please Enter Location Player %d: ",player);
+		scanf("%i",&location);
 		
+		printf("Please ReEnter Location: \n");
+		scanf("%c",&locationchar);
 
-		printf("\nlocation: %i",location);
-		printf("\nlocationstring: %s",locationstring);
 
 		if (board[location] == '-' && location <= 9){
 		
@@ -50,53 +44,45 @@ int makeMove(char board[9],int player, char player1Moves[9], char player2Moves[9
 		} 
 	}
 	
-
-	printf("RUNNNING");	
-
 	char toPlace;
 	if (player == 1){
 	toPlace = 'x';}
-	
-	printf("location: %i",location);
-
-	//sprintf(locationstring,"%d",location);
-	
-	//itoa(location,locationstring,1);
-
-	//printf("locationstring: %c",locationstring);
 
 	int i;
 
-	int ii;
-	if (player==1){
-	
 	for (i=0; i<=9; i++){
 
 		if (player1Moves[i] == '-'){
 		
-			player1Moves[i] = locationstring;
+			player1Moves[i] = locationchar;
 			break; 
 		}
-	}}
+	}
+	printf("\n%s\n",player1Moves);
 	
+///////// ADD APPENDING MOVE TO PLAYER LIST
+///////// ADD so it knows when someone wins
+
 
 	if (player == 2){
 	toPlace = 'o';
 	
-	for (ii=0; ii<=9; ii++){
-		
-		printf("playerwmoves: %c",player2Moves[ii]);
+	for (i=0; i<=9; i++){
 
-                if (player2Moves[ii] == '-'){
-			printf("Player 2 Moves: %i",player2Moves[ii]);
-                        player2Moves[ii] = locationstring;
-			printf("Player 2 Moves after: %i",player2Moves[ii]);
+                if (player2Moves[i] == '-'){
+
+                        player2Moves[i] = locationchar;
                         break;
-                }}}
+                }
+	
+	}
+
+	}
+
 
 	board[location] = toPlace;
 	
-	bool win = false;
+	int win = 0;
 	int winner = 0 ;
 
 	if (player == 1){
@@ -112,13 +98,9 @@ int makeMove(char board[9],int player, char player1Moves[9], char player2Moves[9
 	return winner;
 	}}
 int checkIfWin(char playerMoves[9]){
-	printf("\nin check if win\n");
-
-	bool win = 0;
+	
+	bool win = false;
 	int i;
-	int ii;
-	int iii;
-
 	char across012[3] = {'0','1','2'};
 	char across345[3] = {'3','4','5'};
 	char across678[3] = {'6','7','8'};
@@ -131,47 +113,24 @@ int checkIfWin(char playerMoves[9]){
 
 	int count = 0;
 	int player= 0;
-	
-	printf("check if win above for");
-	
-	printf("\n");
-	for (iii=0; iii<=8; iii++){
-		printf("%c",playerMoves[iii]);
-	}
-	printf("\n");
-
-
-	for (i=0; i<=8; i++){
-		
-		for (ii=0; ii<=2; ii++){
-
-			printf("\ncount: %i",count);
-			printf("\nacross012: %c",across012[ii]);
-			printf("\nplayermoves: %c\n",playerMoves[i]);
-
-			if (across012[ii]==playerMoves[i]){
-				count++;
-				printf("\ncount: %i\n",count);
-				printf("\nacross012: %c\n",across012[ii]);
-				printf("\nplayermoves: %c\n",playerMoves[i]);
-				}
-
-			if (count == 3){win=1; break;}
-		}}
+	for (i=0; i<=9; i++){
+		if (across012[0]==playerMoves[i]){count++;}
+		if (count == 3){win=1; break;}
+		}
 	
 
 	return win;
 }
 
 int changePlayer(int player){
-	printf("\nPlayer = %i\n",player);
+	printf("Player = %i\n",player);
 	
 	printf("\nIn Change\n");
 	if (player == 2){player = 1; printf("Change to 1\n");}
 	
 	else {player = 2; printf("Change to 2\n");}
 	
-	printf("\n\nPlayer = %i\n",player);
+	printf("Player = %i\n",player);
 	return player;  
 
 }
@@ -196,17 +155,7 @@ void  main(void)
 	printBoard1D(board);
 	win = makeMove(board,player,player1Moves,player2Moves);
 	player = changePlayer(player);	
-	printf("\nwin = %i\n",win);
-	
-	int i;
-	
-	printf("\nprinting player 1\n");
-
-	for (i=0;i<=9;i++){
-	
-		printf("%c",player1Moves[i]);
-	
-	}
+	printf("%i",win);
 
 	if (win!=0){break;}
 	}
